4731c9e5a101fce98dd04ab3e4ca49096aec44e0
Tuomas Hyötyniemi <tuomas.hyotyniemi@gmail.com>
2025-11-04 15:55:44 +0200

    Initial commit

---
+++ src/main/java/op1/quoestionnaire/QuoestionnaireApplication.java
@@ -0,0 +1,13 @@
+package op1.quoestionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoestionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoestionnaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/op1/quoestionnaire/QuoestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package op1.quoestionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoestionnaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
