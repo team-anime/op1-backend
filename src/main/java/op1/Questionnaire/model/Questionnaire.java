a3e835d909026a4b519b5eb079d7d2de1f1ad3a2
Matimane <1matias.sainio@gmail.com>
2025-11-11 15:19:28 +0200

    -

--- src/main/java/op1/Questionnaire/model/Questionnaire.java
+++ src/main/java/op1/Questionnaire/model/Questionnaire.java
@@ -1,10 +1,13 @@
 package op1.Questionnaire.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Questionnaire {
@@ -12,6 +15,9 @@ public class Questionnaire {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "questionnaire", cascade = CascadeType.ALL)
+    private List<Question> questions;
 
     public Questionnaire() {
 
@@ -31,4 +37,7 @@ public class Questionnaire {
     public void setName(String name) {
         this.name = name;
     }
+    public List<Question> getQuestions() { return questions; }
+    public void setQuestions(List<Question> questions) { this.questions = questions; }
+
 }
