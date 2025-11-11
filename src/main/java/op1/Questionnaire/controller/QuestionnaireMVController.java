89597489a290ab6a5c20884542a2d2fdd7d8a56f
Elias Jungman <elias.jungman@gmail.com>
2025-11-11 15:46:30 +0200

    -

--- src/main/java/op1/Questionnaire/controller/QuestionnaireMVController.java
+++ src/main/java/op1/Questionnaire/controller/QuestionnaireMVController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 import op1.Questionnaire.model.Questionnaire;
 import op1.Questionnaire.model.QuestionnaireRepository;
@@ -16,16 +17,27 @@ public class QuestionnaireMVController{
     public QuestionnaireMVController(QuestionnaireRepository questionnaireRepository) {
         this.questionnaireRepository = questionnaireRepository;
     }
+    @RequestMapping(value = {"/", "/index"})
+    public String index() {
+        return "index";
+    }
+    
 
     @RequestMapping("/addQuestionnaire")
     public String addQuestionnaire(@RequestParam(name="name", required=false) String name) {
-        if(name != null) {
+        if(name != "" && name != null) {
             Questionnaire questionnaire = new Questionnaire();
             questionnaire.setName(name);
             questionnaireRepository.save(questionnaire);
-            return "index";
+            return "redirect:index";
         }
         return "addQuestionnaire";
     }
+
+    // @RequestMapping(value = "/addQuestionnaire", method = RequestMethod.POST)
+    // public String addQuestionnaire(Model model){
+    // 	model.addAttribute("questionnaire", new Questionnaire());
+    //     return "addQuestionnaire";
+    // }
 
 }
