5a153795e41702541a3dd4fceef0fb5ccd8fc530
Elias Jungman <elias.jungman@gmail.com>
2025-11-18 16:08:47 +0200

    -

--- src/main/java/op1/Questionnaire/controller/RESTController.java
+++ src/main/java/op1/Questionnaire/controller/RESTController.java
@@ -4,28 +4,46 @@ import org.springframework.web.bind.annotation.RestController;
 
 import op1.Questionnaire.model.Questionnaire;
 import op1.Questionnaire.model.QuestionnaireRepository;
+import op1.Questionnaire.model.Question;
+import op1.Questionnaire.model.QuestionRepository;
 
+import java.lang.foreign.Linker.Option;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @RestController
 public class RESTController {
     private final QuestionnaireRepository questionnaireRepository;
+    private final QuestionRepository questionRepository;
 
-    public RESTController(QuestionnaireRepository questionnaireRepository) {
+    public RESTController(QuestionnaireRepository questionnaireRepository, QuestionRepository questionRepository) {
         this.questionnaireRepository = questionnaireRepository;
+        this.questionRepository = questionRepository;
     }
 
     @RequestMapping(value = "/questionnaires", method=RequestMethod.GET)
     public List<Questionnaire> questionnaires() {
         return (List<Questionnaire>) questionnaireRepository.findAll();
     }
+
+    @RequestMapping(value = "/questionnaires/{id}", method=RequestMethod.GET)
+    public @ResponseBody List<Question> findQuestions(@PathVariable("id") Long questionnaireId) {
+        Optional<Questionnaire> q = questionnaireRepository.findById(questionnaireId);
+        List<Question> questions = q.get().getQuestions();
+
+        return questions;
+    }
+    
     
 }
